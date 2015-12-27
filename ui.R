b9664ae8d14839fc794c29f223b04f86c212fb00
Francois Rebaudo <francois.rebaudo@ird.fr>
2015-12-27 19:07:20 -0400

    slider input names changed according to function newHist in server.R

--- ui.R
+++ ui.R
@@ -9,23 +9,22 @@ shinyUI(fluidPage(
          tabPanel("Perso.",
             fluidRow(
               column(3,wellPanel("Opciones del grafico:",
-                 sliderInput("bins", "Numero de barras:", min = 1, max = 50, value = 30),
-                 sliderInput("myR_edad","Color R:", min = 0, max = 255, value = 50),
-                 sliderInput("myG_edad","Color G:", min = 0, max = 255, value = 50),
-                 sliderInput("myB_edad","Color B:", min = 0, max = 255, value = 50))
+                 sliderInput("Edadbins", "Numero de barras:", min = 1, max = 50, value = 30),
+                 sliderInput("REdad","Color R:", min = 0, max = 255, value = 50),
+                 sliderInput("GEdad","Color G:", min = 0, max = 255, value = 50),
+                 sliderInput("BEdad","Color B:", min = 0, max = 255, value = 50))
               ),
-              
               column(9,plotOutput("plotEdad"),downloadButton('downloadPlotEdad','Descargar')
               )
             ),
             fluidRow(
-              column(4,wellPanel("Opciones del grafico:",
+              column(3,wellPanel("Opciones del grafico:",
                  sliderInput("myR_sexo","Color R:", min = 0, max = 255, value = 50),
                  sliderInput("myG_sexo","Color G:", min = 0, max = 255, value = 50),
                  sliderInput("myB_sexo","Color B:", min = 0, max = 255, value = 50))
               ),
               column(4,plotOutput("plotSexo"),downloadButton('downloadPlotSexo','Descargar')),
-              column(4,plotOutput("plotSexoEdad"),downloadButton('downloadPlotSexoEdad','Descargar'))
+              column(5,plotOutput("plotSexoEdad"),downloadButton('downloadPlotSexoEdad','Descargar'))
             )
          ),
          tabPanel("Smartphone",
