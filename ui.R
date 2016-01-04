41ca43226b6430fa221cc1d7bbbf2ac86ec7cb51
Francois Rebaudo <francois.rebaudo@ird.fr>
2016-01-04 12:02:56 -0400

    functions passed into ui_fun.R

--- ui.R
+++ ui.R
@@ -1,20 +1,6 @@
 library(shiny)
 
-#' Make an histogram in shiny-ui.
-#' 
-#' \code{dispNewHist} allows to display an histogram in shiny within a \code{fluidPage} with some basic options on the right panel.
-dispNewHist<-function(plotName,isBins=TRUE,isRGB=TRUE,isDownload=TRUE,optColWidth=3,plotColWidth=9,mtitle="",binsTitle="Number of bars",rTitle="R:",gTitle="G:",bTitle="B:",downloadTitle="Download"){
-  return(fluidRow(
-    column(optColWidth,wellPanel(mtitle,
-      if(isBins==TRUE){sliderInput(paste0(plotName,"bins"), binsTitle, min = 1, max = 50, value = 30)},
-      if(isRGB==TRUE){sliderInput(paste0("R",plotName),rTitle, min = 0, max = 255, value = 50)},
-      if(isRGB==TRUE){sliderInput(paste0("G",plotName),gTitle, min = 0, max = 255, value = 50)},
-      if(isRGB==TRUE){sliderInput(paste0("B",plotName),bTitle, min = 0, max = 255, value = 50)}
-    )
-    ),
-    if(isDownload==TRUE){column(plotColWidth,plotOutput(paste0("plot",plotName)),downloadButton(paste0("downloadPlot",plotName),downloadTitle))}
-  ))
-}
+source("ui_fun.R")
 
 
 # Define UI for application that draws a barplot
