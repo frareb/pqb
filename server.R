69ab02610f89a3979b951fb200fe38c00ed517bb
Francois Rebaudo <francois.rebaudo@ird.fr>
2016-01-04 12:15:30 -0400

    Graphs by QS

--- server.R
+++ server.R
@@ -47,4 +47,33 @@ shinyServer(function(input, output) {
     par(mar=pyramid.plot(xx.pop,xy.pop,lxcol=mcol,rxcol=fcol,gap=0.5,show.values=FALSE,labels=agelabels))
   })
   
+  ### Reactive UI
+  output$reacDepartamento <- renderUI({
+    departamentoList =  unique(as.character(cuest$seccion_a.departamento))
+    selectInput("departamento", "Departamento", departamentoList)
+  })
+  output$reacMunicipio <- renderUI({
+    municipioList =  unique(as.character(cuest$seccion_a.municipio[cuest$seccion_a.departamento == input$departamento]))
+    selectInput("municipio", "Municipio", municipioList)
+  })
+  output$barPlotAfiliacion <- renderPlot({
+    if(input$variables == "afiliacion"){
+      dataMunicipio <- cuest[cuest$seccion_a.municipio == input$municipio,]
+      asociacion<-length(dataMunicipio$seccion_a.afiliaciones.asociacion[dataMunicipio$seccion_a.afiliaciones.asociacion == TRUE])
+      empresa<-length(dataMunicipio$seccion_a.afiliaciones.empresa[dataMunicipio$seccion_a.afiliaciones.empresa == TRUE])
+      indepediente<-length(dataMunicipio$seccion_a.afiliaciones.independiente[dataMunicipio$seccion_a.afiliaciones.independiente == TRUE])
+      camara<-length(dataMunicipio$seccion_a.afiliaciones.camara[dataMunicipio$seccion_a.afiliaciones.camara == TRUE])
+      bp <- cbind(asociacion,empresa,indepediente,camara)
+      par(bg="#ebf0fb", col.axis="#3f4f78",col.lab="#3f4f78", cex=1.2)
+      barplot(bp, names.arg=colnames(bp), col="#3f4f78")
+    }
+    if(input$variables == "parcelas"){
+      dataDepartamento <- data.frame(cuest$seccion_a.municipio[cuest$seccion_a.departamento == input$departamento],cuest$seccion_a.parcela[cuest$seccion_a.departamento == input$departamento])
+      par(bg="#ebf0fb", col.axis="#3f4f78",col.lab="#3f4f78", cex=1.2, mar=c(8,4,4,4))
+      boxplot(as.numeric(dataDepartamento[,2]) ~ as.character(dataDepartamento[,1]), las=2,border="#3f4f78", frame=FALSE, xaxt='n',yaxt='n')
+      axis(1,at=1:length(unique(as.character(dataDepartamento[,1]))),labels=unique(as.character(dataDepartamento[,1])), lwd=0, las=2)
+      axis(2,ylim=seq(from=0, to=max(as.numeric(dataDepartamento[,2])),length.out = 4) ,lwd=1, col="#3f4f78")
+    }
+  })
+  
 })
